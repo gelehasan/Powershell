3d557b81d5703d3001a035cf38f9f3635e89d60e
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-09-20 22:44:56 +0200

    simple powershel function

--- functions.ps1
+++ functions.ps1
@@ -1,1 +1,15 @@
 
+# example of a powershell function
+
+function Invoke-greetings{
+	params(
+		[string]$name
+		)
+		
+		Write-Host "Hello $name!"
+
+}
+
+// When calling the funtion
+
+Invoke-geetings -name "Sam"
