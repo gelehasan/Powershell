48ed6759252987e6564deb18e1e8d26a41ea4dc4
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-09-20 22:45:23 +0200

    example using support should process

--- functions.ps1
+++ functions.ps1
@@ -13,3 +13,20 @@ function Invoke-greetings{
 // When calling the funtion
 
 Invoke-geetings -name "Sam"
+
+
+# an example of using supportshouldprocess
+
+function Invoke-Greetings {
+	[cmdletbinding(SupportShouldProcess)]
+	param(
+		[(Parameter(Mandatory)]
+		[string]$name
+		)
+		
+		forEach( $item in $name){
+				if($PSCmdlet.ShouldProcess($item)){
+					Write-Output "Hello $item" 
+				}
+		}
+}
