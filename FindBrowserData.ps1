f8d38874e63a16ad9e43c71da80200995d897fb9
Gele Hasan <111875403+gelehasan@users.noreply.github.com>
2024-12-05 21:28:47 +0100

    finding browser data

--- FindBrowserData.ps1
+++ FindBrowserData.ps1
@@ -1,1 +1,14 @@
+$Browserdata= "BrowsersInfo.txt"
+function searchForBrowserCredential{
+    $chromePath = "$env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\Login Data"
+    $firefox= "$env:USERPROFILE\APPDATA\Mozilla\Firefox\Prpfiles\*.default-release\logins.json"
 
+    Add-Content -Path $Browserdata -Value "Browser credential"
+    if(Test-Path $chromePath){
+        Add-Content -Path $Browserdata -Value "$chromepath"
+    }
+
+    if(Test-Path $firefox){
+        Add-Content -Path $Browserdata -Value "Found firefox data: $firefox"
+    }
+}
